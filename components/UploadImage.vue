beac73380019d1b384fc6f1e70d4f6e857ff2712
m-moftahchd <m.moftah@charisma-design.eu>
2021-04-16 05:00:42 +0200

    first commit

---
+++ components/UploadImage.vue
@@ -0,0 +1,40 @@
+<template>
+  <label
+    class="flex items-center w-full my-2 rounded cursor-pointer hover:bg-gray-100"
+  >
+    <img width="100" class="bg-gray-100 rounded" v-if="img" :src="img" alt="" />
+    <img
+      width="100"
+      class="p-6 bg-gray-100 rounded"
+      v-else
+      src="https://image.flaticon.com/icons/png/512/45/45141.png"
+      alt=""
+    />
+    <input
+      type="file"
+      @change="selectImage($event.target)"
+      hidden
+      accept="image/*"
+    />
+    <p class="px-2 text-sm italic text-gray-500">Click to select image</p>
+  </label>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      img: null,
+    }
+  },
+  methods: {
+    selectImage(img) {
+      this.img = URL.createObjectURL(img.files[0])
+      this.$emit('getImage', img.files[0])
+    },
+  },
+}
+</script>
+
+<style>
+</style> 
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="p-6">
+    <!-- upload image  -->
+    <section>
+      <div>
+        <h2 class="text-gray-700 font-bold capitalize">
+          upload image component
+        </h2>
+        <p class="text-sm text-gray-600">Using label and</p>
+      </div>
+      <UploadImage />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
